6d1feb7debdc78b85d98d80ef7cc7954af4d7191
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2025-02-17 09:53:23 +0500

    ok

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.use(
 );
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
