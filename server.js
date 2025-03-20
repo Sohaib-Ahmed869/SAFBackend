d61f5d2e92e02f280a0fc77c4f595f9b8bc3b312
AdilRazzaq1919 <adilrazzaq1919@gmail.com>
2025-03-21 01:01:08 +0500

    final Done

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 connectDB();
 setupInstallmentProcessingJob();
 // Middleware
-//app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: '*' }));
 
  app.use(cors({
   origin: [
